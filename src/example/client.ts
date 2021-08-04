f63bf2f48e5def86f20c3c3edbc357bae2652a04
Toliak <toliakpurple@gmail.com>
2021-08-04 22:54:32 +0300

    Update to mtasa-lua-types 0.6.0 Closes #31

--- src/example/client.ts
+++ src/example/client.ts
@@ -6,10 +6,16 @@ import { addEventHandler } from 'mtasa-lua-types/types/mtasa/client/function/eve
 import { outputChatBox } from 'mtasa-lua-types/types/mtasa/client/function/output';
 import { getTime } from 'mtasa-lua-types/types/mtasa/client/function/world';
 import { resourceRoot } from 'mtasa-lua-types/types/mtasa/client/variables';
+import { EventNames } from 'mtasa-lua-types/types/mtasa/client/event/all_event_names';
+import { Event } from 'mtasa-lua-types/types/mtasa/client/mtasa';
 
-addEventHandler('onClientResourceStart', resourceRoot, function() {
-    const [hours, minutes] = getTime();
+addEventHandler<Event.OnClientResourceStart>(
+    EventNames.OnClientResourceStart,
+    resourceRoot,
+    function () {
+        const [hours, minutes] = getTime();
 
-    outputChatBox('TypeScript Resource Boilerplate works.' +
-        `In-game time: ${hours}:${minutes}`);
-});
+        outputChatBox('TypeScript Resource Boilerplate works.' +
+            `In-game time: ${hours}:${minutes}`);
+    },
+);
